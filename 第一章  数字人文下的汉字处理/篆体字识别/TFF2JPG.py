6cd13398bd767fb3b68aa10ee17a27ae899f68ce
thetaaaaa <justice528@foxmail.com>
2022-06-30 14:15:53 +0800

    完善代码

--- 第一章  数字人文下的汉字处理/篆体字识别/TFF2JPG.py
+++ 第一章  数字人文下的汉字处理/篆体字识别/TFF2JPG.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from PIL import Image, ImageFont, ImageDraw
 from zhon.hanzi import characters
 import re
@@ -8,8 +7,7 @@ from fontTools.ttLib import TTFont
 
 def char_to_img(all_chara, img_dir, uniMap, font, img_size):
     """借由汉字列表（all_chara）中的汉字，生成由指定字体构成的图片"""
-
-	for chara in all_chara:
+    for chara in all_chara:
         # 判断是否存在该字
         if ord(chara) in uniMap:
             # 新建长宽为300像素，背景色为白色的画布对象
@@ -19,159 +17,34 @@ def char_to_img(all_chara, img_dir, uniMap, font, img_size):
             draw.text((0, 0), chara, fill="#000", font=font)
             # 获取图像中非零区域边界并裁剪
             im = im.crop(im.getbbox())
-	# 保存汉字图像
-	if not os.path.exists(img_dir + "/" + chara + "/"):
-		os.mkdir(img_dir + "/" + chara + "/")
-	save_path = img_dir + "/" + chara + "/" + str(len(os.listdir(img_dir + "/" + chara + "/"))) + ".png"
-	im.save(save_path)
-
-
-# def char_to_img(chara, ttf_path, img_dir_path):
-# 	im = Image.new("RGB", (1600, 1600), "white")
-
-# 	draw = ImageDraw.Draw(im)
-
-# 	# use a truetype font
-# 	font = ImageFont.truetype(ttf_path, 1600)
-
-# 	#判断是否存在该字
-# 	fontmap = TTFont(ttf_path)
-# 	uniMap = fontmap['cmap'].tables[0].ttFont.getBestCmap()
-
-# 	# 存在该字
-# 	if ord(chara) in uniMap:
-# 		print(chara + " exists")
-# 		draw.text((0, 0), chara, fill="#000" ,font=font)
-
-# 		# remove unneccessory whitespaces if needed
-# 		im=im.crop(im.getbbox())
-
-# 		# write into file
-# 		if os.path.exists(img_dir_path + "/" + chara + "/") != True:
-# 				os.mkdir(img_dir_path + "/" + chara + "/")
-
-# 		save_path = img_dir_path + "/" + chara + "/" + str(len(os.listdir(img_dir_path + "/" + chara + "/"))) + ".png"
-# #         print(save_path)
-# 		im.save(img_dir_path + "/" + chara + "/" + str(len(os.listdir(img_dir_path + "/" + chara + "/"))) + ".png")
-
-
+            # 保存汉字图像
+            if not os.path.exists(img_dir + "/" + chara + "/"):
+                os.mkdir(img_dir + "/" + chara + "/")
+            save_path = img_dir + "/" + chara + "/" + str(len(os.listdir(img_dir + "/" + chara + "/"))) + ".png"
+            im.save(save_path)
 
 
 if __name__ == '__main__':
-	TTF_DIR = "../ALL-Font"
-	img_dir = "../data"
-	img_size = 300 #生成图片像素大小
+    TTF_DIR = "../ALL-Font"
+    img_dir = "../data"
+    img_size = 300  # 生成图片像素大小
 
-	# 判断是否存在文件夹，若否，则创建
-	if not os.path.exists(img_dir):
-		os.makedirs(img_dir)
+    # 判断是否存在文件夹，若否，则创建
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
 
-	all_chara = [chr(i) for i in range(19968,26000)] # 5000个汉字
+    all_chara = [chr(i) for i in range(19968, 26000)]  # 5000个汉字
 
+    for each_font in os.listdir(TTF_DIR):
+        # ttf 字体文件存储路径
+        ttf_path = TTF_DIR + "/" + each_font
+        print("********" + ttf_path + "*****************")
 
-	for each_font in os.listdir(TTF_DIR):
-		# ttf 字体文件存储路径
-		ttf_path = TTF_DIR + "/" + each_font
-		print("********" + ttf_path + "*****************")
+        # 创建Int型unicode拜尼马与字符映射表
+        fontmap = TTFont(ttf_path)
+        uniMap = fontmap['cmap'].tables[0].ttFont.getBestCmap()
 
-		# 创建Int型unicode拜尼马与字符映射表
-		fontmap = TTFont(ttf_path)
-		uniMap = fontmap['cmap'].tables[0].ttFont.getBestCmap()
+        # 加载并创建指定大小的字体对象
+        font = ImageFont.truetype(ttf_path, image_size)
 
-		# 加载并创建指定大小的字体对象
-		font = ImageFont.truetype(ttf_path, image_size)
-
-		char_to_img(all_chara, img_dir, uniMap, font, img_size)
-
-
-
-
-=======
-from PIL import Image, ImageFont, ImageDraw
-from zhon.hanzi import characters
-import re
-import os
-from fontTools.ttLib import TTFont
-
-
-def char_to_img(all_chara, img_dir, uniMap, font, img_size):
-    """借由汉字列表（all_chara）中的汉字，生成由指定字体构成的图片"""
-
-	for chara in all_chara:
-        # 判断是否存在该字
-        if ord(chara) in uniMap:
-            # 新建长宽为300像素，背景色为白色的画布对象
-            im = Image.new("RGB", (img_size, img_size), "white")
-            draw = ImageDraw.Draw(im)
-            # 从画布的坐标（0, 0）处绘制黑色汉字文本
-            draw.text((0, 0), chara, fill="#000", font=font)
-            # 获取图像中非零区域边界并裁剪
-            im = im.crop(im.getbbox())
-	# 保存汉字图像
-	if not os.path.exists(img_dir + "/" + chara + "/"):
-		os.mkdir(img_dir + "/" + chara + "/")
-	save_path = img_dir + "/" + chara + "/" + str(len(os.listdir(img_dir + "/" + chara + "/"))) + ".png"
-	im.save(save_path)
-
-
-# def char_to_img(chara, ttf_path, img_dir_path):
-# 	im = Image.new("RGB", (1600, 1600), "white")
-
-# 	draw = ImageDraw.Draw(im)
-
-# 	# use a truetype font
-# 	font = ImageFont.truetype(ttf_path, 1600)
-
-# 	#判断是否存在该字
-# 	fontmap = TTFont(ttf_path)
-# 	uniMap = fontmap['cmap'].tables[0].ttFont.getBestCmap()
-
-# 	# 存在该字
-# 	if ord(chara) in uniMap:
-# 		print(chara + " exists")
-# 		draw.text((0, 0), chara, fill="#000" ,font=font)
-
-# 		# remove unneccessory whitespaces if needed
-# 		im=im.crop(im.getbbox())
-
-# 		# write into file
-# 		if os.path.exists(img_dir_path + "/" + chara + "/") != True:
-# 				os.mkdir(img_dir_path + "/" + chara + "/")
-
-# 		save_path = img_dir_path + "/" + chara + "/" + str(len(os.listdir(img_dir_path + "/" + chara + "/"))) + ".png"
-# #         print(save_path)
-# 		im.save(img_dir_path + "/" + chara + "/" + str(len(os.listdir(img_dir_path + "/" + chara + "/"))) + ".png")
-
-
-
-
-if __name__ == '__main__':
-	TTF_DIR = "../ALL-Font"
-	img_dir = "../data"
-	img_size = 300 #生成图片像素大小
-
-	# 判断是否存在文件夹，若否，则创建
-	if not os.path.exists(img_dir):
-		os.makedirs(img_dir)
-
-	all_chara = [chr(i) for i in range(19968,26000)] # 5000个汉字
-
-
-	for each_font in os.listdir(TTF_DIR):
-		# ttf 字体文件存储路径
-		ttf_path = TTF_DIR + "/" + each_font
-		print("********" + ttf_path + "*****************")
-
-		# 创建Int型unicode拜尼马与字符映射表
-		fontmap = TTFont(ttf_path)
-		uniMap = fontmap['cmap'].tables[0].ttFont.getBestCmap()
-
-		# 加载并创建指定大小的字体对象
-		font = ImageFont.truetype(ttf_path, image_size)
-
-		char_to_img(all_chara, img_dir, uniMap, font, img_size)
-
-
-
-
->>>>>>> d588d69 (提交文件)
+        char_to_img(all_chara, img_dir, uniMap, font, img_size)
