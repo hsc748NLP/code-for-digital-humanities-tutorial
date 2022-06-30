d18206c1254ceff4cc379f9e9070f187d34211b5
thetaaaaa <justice528@foxmail.com>
2022-06-30 15:31:49 +0800

    update code

--- 第一章  数字人文下的汉字处理/篆体字识别/DeleteBlankIMG.py
+++ 第一章  数字人文下的汉字处理/篆体字识别/DeleteBlankIMG.py
@@ -1,48 +1,32 @@
+import os
+import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
+
 def clean_blank(image_path):
     # 解决全白图片的方案：
     for each_cls in os.listdir(img_dir):
-        dir_path = os.path.join(img_dir,each_cls)
+        dir_path = os.path.join(img_dir, each_cls)
         for each_img in os.listdir(dir_path):
-            image_path = os.path.join(dir_path,each_img)
+            image_path = os.path.join(dir_path, each_img)
             # 1、加载图片
             image = tf.keras.preprocessing.image.load_img(image_path)
-            #将图片转化为numpy的ndarray类型
-            input_arr = keras.preprocessing.image.img_to_array(image)
+            # 将图片转化为numpy的ndarray类型
+            input_arr = tf.keras.preprocessing.image.img_to_array(image)
             input_arr = np.array([input_arr])  # Convert single image to a batch.
             # 2、判断上面的 input_arr的每个元素是否相同 —— 每个像素的RGB数值是否相同
             ele = input_arr[0]
             for i in range(len(input_arr)):
-                if ele != input_arr[i]:
+                if ele.all() != input_arr[i].all():
                     # 非空白图片，跳出循环
                     break
                 if i == len(input_arr) - 1:
                     # 空白图片，删除
                     os.remove(image_path)
-                  
-def clean_blank(image_path):
-    # 解决全白图片的方案：
-    for each_cls in os.listdir(img_dir):
-        dir_path = os.path.join(img_dir,each_cls)
-        for each_img in os.listdir(dir_path):
-            image_path = os.path.join(dir_path,each_img)
-            # 1、加载图片
-            image = tf.keras.preprocessing.image.load_img(image_path)
-            #将图片转化为numpy的ndarray类型
-            input_arr = keras.preprocessing.image.img_to_array(image)
-            input_arr = np.array([input_arr])  # Convert single image to a batch.
-            # 2、判断上面的 input_arr的每个元素是否相同 —— 每个像素的RGB数值是否相同
-            ele = input_arr[0]
-            for i in range(len(input_arr)):
-                if ele != input_arr[i]:
-                    # 非空白图片，跳出循环
-                    break
-                if i == len(input_arr) - 1:
-                    # 空白图片，删除
-                    os.remove(image_path) 
 
 
 if __name__ == '__main__':
-    img_dir = "../data" # 生成的字体图片
+    img_dir = "./data"  # 生成的字体图片路径
 
-    for each_imge in os.listdir(img_dir):
+    for each_imge in tqdm(os.listdir(img_dir)):
         clean_blank(os.path.join(img_dir, each_imge))
--- 第一章  数字人文下的汉字处理/篆体字识别/TFF2JPG.py
+++ 第一章  数字人文下的汉字处理/篆体字识别/TFF2JPG.py
@@ -1,8 +1,7 @@
 from PIL import Image, ImageFont, ImageDraw
-from zhon.hanzi import characters
-import re
 import os
 from fontTools.ttLib import TTFont
+
 
 def char_to_img(all_chara, img_dir, uniMap, font, img_size):
     """借由汉字列表（all_chara）中的汉字，生成由指定字体构成的图片"""
@@ -24,11 +23,11 @@ def char_to_img(all_chara, img_dir, uniMap, font, img_size):
 
 
 if __name__ == '__main__':
-    TTF_DIR = "../ALL-Font"
-    img_dir = "../data"
+    TTF_DIR = "./ALL-Font"
+    img_dir = "./data"
     img_size = 300  # 生成图片像素大小
 
-    # 判断是否存在文件夹，若否，则创建
+    # 判断是否存在文件夹，若不存在，则创建
     if not os.path.exists(img_dir):
         os.makedirs(img_dir)
 
@@ -37,13 +36,10 @@ if __name__ == '__main__':
     for each_font in os.listdir(TTF_DIR):
         # ttf 字体文件存储路径
         ttf_path = TTF_DIR + "/" + each_font
-        print("********" + ttf_path + "*****************")
-
+        print("Processing:", ttf_path)
         # 创建Int型unicode拜尼马与字符映射表
         fontmap = TTFont(ttf_path)
         uniMap = fontmap['cmap'].tables[0].ttFont.getBestCmap()
-
         # 加载并创建指定大小的字体对象
-        font = ImageFont.truetype(ttf_path, image_size)
-
+        font = ImageFont.truetype(ttf_path, img_size)
         char_to_img(all_chara, img_dir, uniMap, font, img_size)
